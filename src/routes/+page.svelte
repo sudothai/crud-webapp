f09a7970139c5ad06ae594b1ca25f44bd6f796ae
Thai Radin <thai.r.360@gmail.com>
2023-12-08 11:40:50 -0500

    basic html

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="grid">
+	<div>
+		<h2>Articles:</h2>
+			<article>
+				<header>Example Article</header>
+				<p>
+					This is an article content!
+				</p>
+                <button class="outline secondary">Delete Article</button>
+				<a href="/" role="button" class="outline constrast" style="width: 100%;"
+					>Edit Article</a
+				>
+			</article>
+	</div>
+	<form action="" method="POST">
+		<h3>New Article</h3>
+		<label for="title"> Title </label>
+		<input type="text" id="title" name="title" />
+		<label for="title"> Title </label>
+		<textarea id="content" name="content" rows={5} />
+		<button type="submit">Add Article</button>
+	</form>
+</div>
