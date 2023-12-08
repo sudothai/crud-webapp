5460fb768751bf2c6ec14b7e5df05a5706a7c69c
Thai Radin <thai.r.360@gmail.com>
2023-12-08 12:22:15 -0500

    forgot a line

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,8 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import type { PrismaClient } from "@prisma/client"
+
 declare global {
 	namespace App {
 		// interface Error {}
